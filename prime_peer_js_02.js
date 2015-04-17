f86e6642993c7bab9cc070cec75e62c0c5b7ca12
5nahalf <5nahalf@comcast.net>
2015-04-17 15:50:13 -0500

    mod js file

--- prime_peer_js_02.js
+++ prime_peer_js_02.js
@@ -1,4 +1,27 @@
 
+function People(name, empNum, pay, dept){
+	this.name = name;
+	this.empNum = empNum;
+	this.pay = pay;
+	this.dept = dept;
+
+	this.magic = function() {
+		return this.pay + this.dept / this.empNum + this.name;
+	};
+	this.log = function() {
+		console.log(this.magic());
+	};
+}
+
+var array = [];
+
+for(i = 0; i <=20; i++){
+	var obj = new People(i, randomNumber(10, 50), randomNumber(1, 10), 54)
+	obj.log();
+	array.push(obj);
+};
+
+
 //Code goes here
 
 //Utility function
@@ -6,3 +29,4 @@
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
+
